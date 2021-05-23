c1a2a72d9aba4f2cb8b9c4e260bd4db212604063
MasterJ-06 <jonathan.miller@live.com.au>
2021-05-23 17:33:17 +1000

    Changes to CORS

--- src/index.js
+++ src/index.js
@@ -7,6 +7,24 @@ const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000, https://masterj-library-app.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(userRouter)
 app.use(bookRouter)
 
