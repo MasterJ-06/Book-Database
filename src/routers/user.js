e66166756dff0d32353c4e4a004b0dd805a90f6c
MasterJ-06 <71957127+MasterJ-06@users.noreply.github.com>
2022-06-14 14:17:20 +1000

    debuging

--- src/routers/user.js
+++ src/routers/user.js
@@ -50,7 +50,7 @@ router.post('/users/refresh', async (req, res) => {
     try {
         const user = await User.findByJWT(req.body.jwt)
         //const token = await user.generateAuthRefreshToken()
-        res.send({ user })
+        res.send(user)
     } catch (e) {
         res.status(400).send()
     }
